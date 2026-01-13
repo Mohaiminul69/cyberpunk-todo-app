730e21ed808e19611fe4a9f81494a3e26ba1fbe5
mohaiminul69 <mohaiminul69@gmail.com>
2026-01-13 23:59:45 +0600

    card style updated.

--- src/components/task_card/task_card.jsx
+++ src/components/task_card/task_card.jsx
@@ -7,10 +7,6 @@ const TaskCard = ({ task, taskNumber, setTasks }) => {
     id: task.id,
   });
 
-  const style = {
-    opacity: isDragging ? 0 : 1,
-  };
-
   const handleTaskDelete = () => {
     setTasks((prevTasks) => {
       const newList = prevTasks.filter((t) => t.id !== task.id);
@@ -21,14 +17,16 @@ const TaskCard = ({ task, taskNumber, setTasks }) => {
 
   return (
     <div
-      style={style}
       ref={setNodeRef}
       {...listeners}
       {...attributes}
       onMouseEnter={playHoverSound}
       className={`card--${task.status} card--wrapper clipped-card card-shadow`}
     >
-      <div className="card--border card-shadow clipped-card">
+      <div
+        style={{ opacity: isDragging ? 0.5 : 1 }}
+        className="card--border card-shadow clipped-card"
+      >
         <div className="pad text-shadow--glow card">
           # {taskNumber} {task.task}
           <button onClick={handleTaskDelete} className="task-delete--button">
